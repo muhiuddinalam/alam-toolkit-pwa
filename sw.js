f260dd7452428c75dbbc87b4a8f73fa2bcc6d52d
muhiuddinalam <61884073+muhiuddinalam@users.noreply.github.com>
2026-01-30 01:54:09 +0600

    Add caching functionality to Service Worker
    Implement caching for PWA assets in Service Worker

--- sw.js
+++ sw.js
@@ -1,4 +1,23 @@
-// Ultra simple Service Worker
-self.addEventListener('fetch', function(event) {
-  // Do nothing - just enable PWA
+// Main PWA Service Worker
+const CACHE_NAME = 'alamtoolkit-v1';
+const urlsToCache = [
+  '/',
+  '/habit-and-goal-tracker.html',
+  '/style.css',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
