cc958992d8940f9704d08b779c4d6f060a22316a
muhiuddinalam <61884073+muhiuddinalam@users.noreply.github.com>
2026-01-30 23:53:40 +0600

    Refactor service worker for caching and notifications

--- sw.js
+++ sw.js
@@ -1,44 +1,53 @@
 const CACHE_NAME = 'alamtoolkit-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/firebase-messaging-sw.js',
-  '/icon-192.png',
-  '/icon-512.png'
+    '/',
+    '/index.html',
+    '/habit-and-goal-tracker.html',
+    '/icon-192.png',
+    '/icon-512.png',
+    '/manifest.json'
 ];
 
 // Install Service Worker
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+            .then(() => self.skipWaiting())
+    );
 });
 
 // Activate Service Worker
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => {
+            console.log('Service Worker activated');
+            return self.clients.claim();
         })
-      );
-    }).then(() => self.clients.claim())
-  );
+    );
 });
 
 // Fetch with Cache
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached version or fetch from network
+                return response || fetch(event.request);
+            })
+    );
 });
 
 // Background sync for offline reminder scheduling
@@ -114,4 +123,5 @@ self.addEventListener('notificationclick', event => {
     );
   }
 });
+
 
