166d6dff21305e4157982e76544b66be176cf288
muhiuddinalam <61884073+muhiuddinalam@users.noreply.github.com>
2026-01-31 18:42:29 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,10 +14,3 @@ self.addEventListener('install', event => {
       .then(cache => cache.addAll(urlsToCache))
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
